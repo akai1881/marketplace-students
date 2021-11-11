7fc5053c5f5409bbcb24212a4a9588cdfdbd4f84
York5 <mikhailvishin2@yandex.com>
2021-11-11 21:35:43 +0600

    Add DetailView with Carousel

--- src/components/Products/ProductsDetails.js
+++ src/components/Products/ProductsDetails.js
@@ -1,11 +1,114 @@
-import React from 'react';
+import { Grid, makeStyles, Paper } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
+import { useProducts } from '../../contexts/ProductsContext';
+import MySpinner from '../../shared/MySpinner';
+import {
+  ImageWithZoom,
+  Slider,
+  CarouselProvider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
+const useStyles = makeStyles((theme) => ({
+  custom_container: {
+    marginTop: '60px',
+    alignItems: 'center',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary,
+    textAlign: 'left',
+  },
+}));
 
 const ProductsDetails = () => {
-    return (
-        <div>
-            <h1>Products details</h1>
-        </div>
-    );
+  const { fetchOneProduct, productDetails } = useProducts();
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchOneProduct(id);
+  }, []);
+
+  const classes = useStyles();
+  return (
+    <Grid container>
+      {productDetails ? (
+        <Grid container className={classes.custom_container}>
+          <Grid item md={4}>
+            <CarouselProvider
+              naturalSlideWidth={100}
+              naturalSlideHeight={125}
+              totalSlides={3}
+            >
+              <Slider>
+                <Slide index={0}>
+                  <ImageWithZoom src={productDetails.image} />
+                </Slide>
+                <Slide index={1}>
+                  <ImageWithZoom src={productDetails.image} />
+                </Slide>
+                <Slide index={2}>
+                  <ImageWithZoom src={productDetails.image} />
+                </Slide>
+              </Slider>
+              <ButtonBack>Back</ButtonBack>
+              <ButtonNext>Next</ButtonNext>
+            </CarouselProvider>
+          </Grid>
+          <Grid item md={6}>
+            <Paper elevation={3} className={classes.paper}>
+              <table>
+                <tbody>
+                  <tr>
+                    <th>Title:</th>
+                    <td>{productDetails.title}</td>
+                  </tr>
+                  <tr>
+                    <th>Price:</th>
+                    <td>{productDetails.price}</td>
+                  </tr>
+
+                  {productDetails.salePrice ? (
+                    <tr>
+                      <th>Sale Price:</th>
+                      <td>{productDetails.salePrice}</td>
+                    </tr>
+                  ) : null}
+
+                  <tr>
+                    <th>Description:</th>
+                    <td>{productDetails.description}</td>
+                  </tr>
+                  <tr>
+                    <th>Phone:</th>
+                    <td>{productDetails.phone}</td>
+                  </tr>
+                  <tr>
+                    <th>Author:</th>
+                    <td>{productDetails.author}</td>
+                  </tr>
+                  <tr>
+                    <th>Category:</th>
+                    <td>{productDetails.category}</td>
+                  </tr>
+                  <tr>
+                    <th>Count in Stock:</th>
+                    <td>{productDetails.countInStock}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </Paper>
+          </Grid>
+        </Grid>
+      ) : (
+        <MySpinner />
+      )}
+    </Grid>
+  );
 };
 
 export default ProductsDetails;
--- src/contexts/ProductsContext.js
+++ src/contexts/ProductsContext.js
@@ -1,104 +1,133 @@
 import React, { createContext, useContext, useReducer } from 'react';
 import { $api } from '../service/axios-config';
 import {
-    GET_PRODUCTS_ERROR,
-    GET_PRODUCTS_LOADING,
-    GET_PRODUCTS_SUCCESS,
-    GET_PRODUCT_ERROR,
-    GET_PRODUCT_LOADING,
-    GET_PRODUCT_SUCCESS,
+  GET_PRODUCTS_ERROR,
+  GET_PRODUCTS_LOADING,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCT_ERROR,
+  GET_PRODUCT_LOADING,
+  GET_PRODUCT_SUCCESS,
 } from '../utils/constants';
-import { productError, productLoading, productSuccess } from './actions/productDetailsActions';
-import { productsError, productsLoading, productsSuccess } from './actions/productsActions';
+import {
+  productError,
+  productLoading,
+  productSuccess,
+} from './actions/productDetailsActions';
+import {
+  productsError,
+  productsLoading,
+  productsSuccess,
+} from './actions/productsActions';
 
 const productsContext = createContext();
 
 export const useProducts = () => useContext(productsContext);
 
 const initialState = {
+  loading: false,
+  error: null,
+  products: [],
+  productDetails: {
     loading: false,
     error: null,
-    products: [],
-    productDetails: {
-        loading: false,
-        error: null,
-        product: null,
-    },
+    product: null,
+  },
 };
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case GET_PRODUCTS_LOADING:
-            return { ...state, loading: true };
+  switch (action.type) {
+    case GET_PRODUCTS_LOADING:
+      return { ...state, loading: true };
 
-        case GET_PRODUCTS_ERROR:
-            return { ...state, loading: false, products: [], error: action.payload };
+    case GET_PRODUCTS_ERROR:
+      return { ...state, loading: false, products: [], error: action.payload };
 
-        case GET_PRODUCTS_SUCCESS:
-            return { ...state, loading: false, error: null, products: action.payload };
+    case GET_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        products: action.payload,
+      };
 
-        case GET_PRODUCT_LOADING:
-            return {
-                ...state,
-                productDetails: { ...state.productDetails, loading: true },
-            };
+    case GET_PRODUCT_LOADING:
+      return {
+        ...state,
+        productDetails: { ...state.productDetails, loading: true },
+      };
 
-        case GET_PRODUCT_SUCCESS:
-            return {
-                ...state,
-                productDetails: { ...state.productDetails, loading: false, error: null, product: action.payload },
-            };
+    case GET_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        productDetails: {
+          ...state.productDetails,
+          loading: false,
+          error: null,
+          product: action.payload,
+        },
+      };
 
-        case GET_PRODUCT_ERROR:
-            return {
-                ...state,
-                productDetails: { ...state.productDetails, loading: false, error: action.payload, product: null },
-            };
+    case GET_PRODUCT_ERROR:
+      return {
+        ...state,
+        productDetails: {
+          ...state.productDetails,
+          loading: false,
+          error: action.payload,
+          product: null,
+        },
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 const ProductsContext = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const fetchProducts = async () => {
-        dispatch(productsLoading());
-        try {
-            const { data } = await $api();
-            setTimeout(() => {
-                dispatch(productsSuccess(data));
-            }, 2000);
-        } catch (error) {
-            console.log(error.message);
-            dispatch(productsError(error.message));
-        }
-    };
+  const fetchProducts = async () => {
+    dispatch(productsLoading());
+    try {
+      const { data } = await $api();
+      setTimeout(() => {
+        dispatch(productsSuccess(data));
+      }, 2000);
+    } catch (error) {
+      console.log(error.message);
+      dispatch(productsError(error.message));
+    }
+  };
 
-    const fetchOneProduct = async (id) => {
-        dispatch(productLoading());
-        try {
-            const { data } = await $api(`/${id}`);
-            dispatch(productSuccess(data));
-        } catch (error) {
-            console.log(error.message);
-            dispatch(productError(error.message));
-        }
-    };
+  const fetchOneProduct = async (id) => {
+    dispatch(productLoading());
+    try {
+      const { data } = await $api(`/${id}`);
+      dispatch(productSuccess(data));
+      //   console.log(data);
+      //   console.log(state);
+    } catch (error) {
+      console.log(error.message);
+      dispatch(productError(error.message));
+    }
+  };
 
-    const values = {
-        products: state.products,
-        loading: state.loading,
-        error: state.error,
-        productDetailsLoading: state.productDetails.loading,
-        productDetails: state.productDetails.product,
-        productDetailsError: state.productDetails.error,
-        fetchProducts,
-        fetchOneProduct,
-    };
+  const values = {
+    products: state.products,
+    loading: state.loading,
+    error: state.error,
+    productDetailsLoading: state.productDetails.loading,
+    productDetails: state.productDetails.product,
+    productDetailsError: state.productDetails.error,
+    fetchProducts,
+    fetchOneProduct,
+  };
 
-    return <productsContext.Provider value={values}>{children}</productsContext.Provider>;
+  return (
+    <productsContext.Provider value={values}>
+      {children}
+    </productsContext.Provider>
+  );
 };
 
 export default ProductsContext;
